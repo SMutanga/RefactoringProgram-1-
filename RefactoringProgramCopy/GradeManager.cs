312f11aa5e651a90b0e8b36fc69266bc7ca2fc1f
Sannah Penelope Azuh Mutanga <penelopemutanga@outlook.com>
2025-11-23 20:32:54 -0600

    Add project files.

---
+++ RefactoringProgramCopy/GradeManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace RefactoringHomework
+{
+    public class GradeManager
+    {
+        public double passingGrade = 50.0;
+
+        public void LogGradeProcessingManager()
+        {
+            Console.WriteLine("Working on grades...");
+        }
+
+        public void processGrades(string studentName, double[] grades)
+        {
+            Console.WriteLine("Processing grades for " + studentName);
+
+            double total = NewMethod(grades);
+
+            double average = total / grades.Length;
+            Console.WriteLine("Average: " + average);
+
+
+            if (average >= passingGrade)
+            {
+                Console.WriteLine("Status: Pass");
+            }
+            else
+            {
+                Console.WriteLine("Status: Fail");
+            }
+        }
+
+        private static double NewMethod(double[] grades)
+        {
+            double total = 0;
+            for (int i = 0; i < grades.Length; i++)
+            {
+                total += grades[i];
+            }
+
+            return total;
+        }
+
+        public double calculateFinalGrade(double bonus, double average)
+        {
+            return average + bonus;
+        }
+    }
+}
+
